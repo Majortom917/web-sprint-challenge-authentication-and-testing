480d50ca8b0a5725c2fe2b7ffec5ba3c4460f8ff
Majortom917 <70175230+Majortom917@users.noreply.github.com>
2021-01-29 10:15:55 -0800

    mvp

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -4,21 +4,26 @@ const Users = require("./auth-model");
 const jwt = require("jsonwebtoken");
 require('dotenv').config();
 
-router.post('/register', async(req, res) => {
+router.post('/register', async(req, res, next) => {
   //res.end('implement register, please!');
   try{
+
     const {username, password} = req.body
+
     const user = await Users.findBy({username}).first()
+
       if(user){
-        return res.status(409).json({message: 'user already exists'})
+        return res.status(409).json({message: 'username taken'})
       }
       if(!username || !password){
         return res.status(409).json({ message: 'username and password required'})
       }
+
       const newUser = await Users.add({
         username, 
         password: await bcrypt.hash(password, 13)
       })
+
       res.status(201).json(newUser)
     
   }catch(err){
@@ -55,15 +60,19 @@ router.post('/login', async (req, res, next) => {
   //res.end('implement login, please!');
   try{
     const {username, password} = req.body
+
     if(!username || !password){
       return res.status(401).json({message: 'username and password required'})
     }
     const user = await Users.findBy({username}).first()
+
     const passwordValid = await bcrypt.compare(password, user.password)
+
     if(!user || !passwordValid){
-      return res.status(401).json({message: 'this is not the droid I am looking for'})
+      return res.status(401).json({message: 'invalid credentials'})
     }
     const token = jwt.sign({id: user.id}, process.env.JWT_SECRET)
+
     res.json({
       message: `welcome, ${user.username}`,
       token:token
--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,42 @@
 // Write your tests here
-test('sanity', () => {
+/* test('sanity', () => {
   expect(true).toBe(false)
+}) */
+const request = require("supertest")
+const server = require("../api/server")
+const db= require("../data/dbConfig")
+beforeEach(async () => {
+  await db.seed.run()
+})
+afterAll(async () => {
+  await db.destroy()
+})
+
+describe('POST /register user', ()=>{
+  it("checks the status code", async () => {
+    await db('users').truncate()
+      const res = await request(server).post("/api/auth/register").send({username: 'Tim', password: '963'})
+      expect(res.status).toBe(201)
+      expect(res.type).toBe("application/json")
+  }) 
+  it("checks the data sent", async () => {
+    await db('users').truncate()
+      const res = await request(server).post("/api/auth/register").send({username: 'Tim', password: '963'})
+      expect(res.body.id).toBe(1)
+      expect(res.body.username).toBe('Tim')
+  })  
+})
+describe('POST /login users', ()=>{
+  test("logs in user", async () => {
+      const res = await request(server).post("/api/auth/login").send({username: 'mark', password: 'pw'})
+      expect(res.statusCode).toBe(200)
+      expect(res.type).toBe("application/json")
+      expect(res.body.message).toBe('Welcome, mark!')
+  })  
+
+  test("rejects user with invalid credentials and gives correct error code", async () => {
+    const res = await request(server).post("/api/auth/login").send({username: 'John', password: '$2a$14$F8VPm/IOSys2zUYP5ja0lObZol/ubBgnJ8YDUhexxskdyfe4CBtQm'})
+    expect(res.statusCode).toBe(401)
+    expect(res.body.message).toBe('invalid credentials')
+}) 
 })
